58563fc383f5592d9d3c4be601ae302f42844661
Msoucy07 <matthew.soucy1@maine.edu>
2025-02-04 11:50:14 -0500

    Create ParkingSpot.java

---
+++ ParkingSpot.java
@@ -0,0 +1,16 @@
+public class ParkingSpot {
+
+    public static boolean is_handicapped = true;
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(Car.make);
+        System.out.println(Car.color);
+        if (Car.handicapped == true){
+            System.out.println("Car is handicapped");
+        }
+    }
+
+}
