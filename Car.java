c6247d7fbe0e885275487c51f449c1d97e1a7e45
Msoucy07 <matthew.soucy1@maine.edu>
2025-02-04 11:50:17 -0500

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,13 @@
+public class Car {
+
+    public static boolean handicapped = true;
+
+    public static String make = "Honda";
+    public static String color = "Black";
+    
+    public static void main(String[] args) {
+        System.out.println(make);
+    }
+
+
+}
