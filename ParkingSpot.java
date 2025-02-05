c67ff929dfff657598a67a644d2f4b3e7a8dc1b7
Msoucy07 <matthew.soucy1@maine.edu>
2025-02-05 15:03:55 -0500

    Update ParkingSpot.java

--- ParkingSpot.java
+++ ParkingSpot.java
@@ -1,6 +1,6 @@
 public class ParkingSpot {
 
-    public static boolean is_handicapped = true;
+    public static boolean is_handicapped;
 
 
 
@@ -8,9 +8,7 @@ public class ParkingSpot {
     public static void main(String[] args) {
         System.out.println(Car.make);
         System.out.println(Car.color);
-        if (Car.handicapped == true){
-            System.out.println("Car is handicapped");
-        }
+        
     }
 
 }
